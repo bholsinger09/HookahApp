09ff3eeffd9c386e7a06a0ad3254081b426e211b
Benjamin Holsinger <benjamin.holsinger@hp.com>
2023-03-30 12:15:22 -0600

    creating welcome body component

--- HookahShop/Components/bodyWelcome.swift
+++ HookahShop/Components/bodyWelcome.swift
@@ -5,4 +5,29 @@
 //  Created by Ben Holsinger on 3/26/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to the Grocery & Hookah house!")
+                .font(.title)
+                .id("welcome")
+            Text("Come to visit our store, we carry the very best shisha, hookah, tobacco, and vape products.")
+                .id("introductionProducts")
+            Text("Come see our new Grocery products such as a variety of Tea and Coffee.")
+                .id("introductionGrocery")
+            HStack {
+                Text("Check our customers review on google here:")
+                Link("Click here", destination: URL(string: "https://www.yelp.com/biz/grocery-house-boise")!)
+            }
+            .id("introductionReview")
+            Text("Free shipping for all Products to all US States")
+                .id("introductionFreeShipping")
+        }
+        .padding()
+        .foregroundColor(.black)
+        .multilineTextAlignment(.center)
+        .lineSpacing(5)
+    }
+}
