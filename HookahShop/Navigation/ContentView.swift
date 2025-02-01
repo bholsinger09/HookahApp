9761588f55ee25e70529238285f2704a9498859e
ben <bholsinger@gmail.com>
2025-02-01 16:03:39 -0700

    added content view

---
+++ HookahShop/Navigation/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HookahShop
+//
+//  Created by Ben H on 2/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
